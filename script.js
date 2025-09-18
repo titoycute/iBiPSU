02833675d4bdb15dc8f6523bb5fca9947692fc07
titoycute <titoycustodio@gmail.com>
2025-09-18 21:10:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1681,11 +1681,14 @@ this.stopDailyRewardTimer = () => {
             <i data-lucide="shield-alert" class="w-16 h-16 mx-auto text-amber-400 mb-4"></i>
             <h2 class="text-2xl font-bold mb-2">Account Pending Approval</h2>
             <p class="text-gray-400">Your account is registered. An administrator will review it shortly, granting full access upon approval.</p>
-            <div style="position: relative; width: 100%; height: 0; padding-top: 177.7778%;padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;border-radius: 8px; will-change: transform;">
-              <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-            src="https://www.canva.com/design/DAGy3ERSjCo/4e2CdzmxIShB6KdG7Jui2w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-            </iframe>
-            </div>
+           <div style="position: relative; width: 100%; height: 0; padding-top: 177.7778%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https://www.canva.com/design/DAGzVdGAtOo/XKZk6a7b42uHmO_zDGrzfQ/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+
         </div>
       `;
       }
